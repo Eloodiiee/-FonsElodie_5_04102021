82c0c95cc0248202d24430067e7ff23b4f15ecc5
Fons Elodie <elodie.fons66@gmail.com>
2021-11-12 09:46:16 +0100

    Finitions js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,11 +1,22 @@
-//------Initialisation du panier----
+let cart  = JSON.parse(localStorage.getItem("cartItems"))||[];
+const cartContent = document.querySelector("#cart__items");
+//------Tentative de detection de doublon dans le panier------
 
-let cart  = JSON.parse(localStorage.getItem("cartItems"));
-const cartContent = document.querySelector("#cart__items");   //----Sélectionne la div "cart__items"----
+for(let article of cart){
+  console.log(article);
+  console.log(article.productId);
+  if(article.productId == cart[0].productId && article.color == cart[0].color){
+    let newQuantity = 0;
+    newQuantity = parseInt(article.quantityProduct)+parseInt(cart[0].quantityProduct);
+    console.log("article :"+ article.quantityProduct);
+    console.log("cart :"+ cart[0].quantityProduct);
+    console.log(newQuantity);
+  }
+}
 
-if(cart === null || cart == 0 ){    //----Boucle if , qui va tester si ce panier existe dans le localstorage OU s'il existe mais qu'il est vide----
-  //-----Déclaration de la constante d'un panier vide et met sur la page que le panier est vide en y ajoutant du style dessus----
-  const emptyCart = `               
+//------Si le panier est vide afficher "le panier est vide"------
+if(cart === null || cart == 0 ){
+  const emptyCart = `
   <style type="text/css">
 .emptyCart {
   border-top: 1px solid white;
@@ -16,16 +27,14 @@ if(cart === null || cart == 0 ){    //----Boucle if , qui va tester si ce panier
 </style>
     <div class="emptyCart">Le panier est vide</div>
   `;
-
-  cartContent.innerHTML = emptyCart;      //------Placement sur la page du code HTML et CSS ci-dessus , sur la DIV "cart_items"------
-  //------Met la quantité et le prix total à zéro----
+  cartContent.innerHTML = emptyCart;
   document.getElementById('totalQuantity').innerHTML = 0;
   document.getElementById('totalPrice').innerHTML = 0;
 }
+//------Sinon remplir le panier------
 else{
-
 let fullCart = [];
-for (let i = 0; i < cart.length; i++) {               //--------Placement des Items du panier sur la page----
+for (let i = 0; i < cart.length; i++) {
     fullCart += ` 
     <article class="cart__item" data-id="${cart[i].productId}">
     <div class="cart__item__img">
@@ -34,7 +43,9 @@ for (let i = 0; i < cart.length; i++) {               //--------Placement des It
   <div class="cart__item__content">
                   <div class="cart__item__content__titlePrice">
                     <h2>${cart[i].name}</h2>
+                    <p>${cart[i].color}</p>
                     <p>${cart[i].price}€</p>
+                    
                   </div>
                   <div class="cart__item__content__settings">
                     <div class="cart__item__content__settings__quantity">
@@ -48,73 +59,115 @@ for (let i = 0; i < cart.length; i++) {               //--------Placement des It
                 </div>
   </article>
   `
-
   let totalItems = 0;
   let totalPriceCart = 0;
-  for (let o = 0; o < cart.length; o++) {  //----------Sert a calculer le nombre total d'items dans le panier, ainsi que le prix total de tout les items----     
+  for (let o = 0; o < cart.length; o++) {
     totalItems += parseInt(cart[o].quantityProduct)
     totalPriceCart += cart[o].totalPrice
   }
-
-}
-//-------Sert a afficher le contenu du panier , le prix total des items, mais aussi le nombre total----
-  document.getElementById("cart__items").innerHTML = fullCart;
-  document.getElementById('totalQuantity').innerHTML = totalItems;
+  cartContent.innerHTML = fullCart;
+  document.getElementById('totalQuantity').innerHTML =  totalItems;
   document.getElementById('totalPrice').innerHTML = totalPriceCart;
   }
+}
+//----------------------------Gestion du changement de quantité des articles et mise à jour du prix--------------------------------------------
 
-
-//----------------------------Gestion de la quantité des articles--------------------------------------------
-
-let quantityField = document.querySelectorAll(".itemQuantity");   //------Sélectionne l'input de quantité----
-
-for (let q = 0; q < quantityField.length; q++){
-  quantityField[q].addEventListener("focusout", (event) =>{   //------Déclenche le code ci-dessous quand on ne focus plus l'input----
+let quantityField = document.querySelectorAll(".itemQuantity"); //------Je sélectionne l'input de quantité ----
+for (let q = 0; q < quantityField.length; q++){ //------Pour chaque input de quantité on vérifie s'il y a un changement de valeur----
+  quantityField[q].addEventListener("input", (event) =>{
     event.preventDefault();
-    let quantityArticles = quantityField[q].value;           //------Déplace la valeur de l'input désormais à jour dans une nouvelle variable-------
-    cart[q].quantityProduct = quantityArticles;     //------On utilise cette variable pour changer la quantité de canapé dans l'objet qui a été généré par product----
-    console.log(cart);
-    cart[q].totalPrice = cart[q].price * cart[q].quantityProduct;  //----Recalcul du prix total avec la nouvelle quantité----
-    localStorage.setItem(
+    let quantityArticles = quantityField[q].value; //-------Attibution de la valeur de l'input dans "quantityArticles"----
+    cart[q].quantityProduct = quantityArticles;//------Mise à jour de la quantité dans le cart.quantityProduct----
+    cart[q].totalPrice = cart[q].price * cart[q].quantityProduct;//------Recalcul du prix total d'un des objet de l'array cartItems----
+    localStorage.setItem( //------Réenregistrement du panier --
       "cartItems",
-      JSON.stringify(cart)      //----Va mettre à jour le cart (panier)----
-  );
-  window.location.href = "cart.html"; //----Rafrîchis la page----
+      JSON.stringify(cart)
+    );
+    console.log(cart);
+    if(cart[q].quantityProduct.length === 0){ //----Vérifie s'il n'y a aucuns caractères dans l'input et le met a zéro si c'est le cas----
+      cart[q].quantityProduct=0;
+    }
+    let totalItems = 0;   //----Boucle qui recalcule le nombres d'items total ainsi que le prix total du panier--
+    let totalPriceCart = 0;
+    for (let u = 0; u < cart.length; u++) {
+      totalItems += parseInt(cart[u].quantityProduct)
+      totalPriceCart += cart[u].totalPrice
+    }
+  document.getElementById('totalQuantity').innerHTML =  totalItems;   //----Affichage des nouvelles valeurs----
+  document.getElementById('totalPrice').innerHTML = totalPriceCart;
   });
 }
-
-
 //----------------------------Gestion du boutton supprimer de l'article--------------------------------------------
 //---------------Sélection des références de tous les boutons suprimer------------------
-let deleteItem = document.querySelectorAll(".deleteItem");
+let deleteItem = document.querySelectorAll(".deleteItem");  
 console.log(deleteItem);
 for (let l = 0; l < deleteItem.length; l++) {
     deleteItem[l].addEventListener("click", (event) => {
     event.preventDefault();
-
-//---- L'id de suppression du produit qui va être supprimé en cliquant sur le bouton ----
+//---- L'id du produit va être supprimé en cliquant sur le bouton 
 let id_cart__item__content__settings__delete = cart[l].idDeletion;
 console.log("cart__item__content__settings__delete");
 console.log(id_cart__item__content__settings__delete);
-
-
-//Avec la méthode filter je sélectionne les éléments à garder et je supprime l'élément où le bouton supprimer a été cliqué----
+//avec la méthode filter je sélectionne les éléments à garder et je supprime l'élément où le bouton supprimer a été cliqué----
 cart = cart.filter(el => el.idDeletion !== id_cart__item__content__settings__delete
 );
 console.log(cart);
-
-
-
-//--------On envoie la variable dans le localstorage------
-//--------La transformation en format JSON et l'envoyer dans la key "cartItems" du localstorage------
+//--------On envoie la variable dans le localstorage
+//--------La transformation en format JSON et l'envoyer dans la key "cartItems" du localstorage
 localStorage.setItem(
     "cartItems",
     JSON.stringify(cart)
 );
-
-//----Alerte pour avertir que le produit a été supprimé et rechargement de la page------
+//alert pour avertir que le produit a été supprimé et rechargement de la page
 alert("Ce produit à bien été supprimé du panier");
 window.location.href = "cart.html";
-
 });
 }
+const btnSendFormular = document.getElementById("order")
+//let btnSendFormular = document.getElementsByClassName("cart__order__form__submit");
+console.log(btnSendFormular);
+
+//-------------- The btn with all the instructions for formular send --------------
+  btnSendFormular.addEventListener("click", (eventSecure) =>{
+    eventSecure.preventDefault();
+    
+  console.log("coucou");
+    //Here is the const to catch all the data's from my formular
+  
+  const order = {
+  contact: {
+  
+  firstName: document.getElementById("firstName").value,
+  lastName: document.getElementById("lastName").value,
+  address: document.getElementById("address").value,
+  city: document.getElementById("city").value,
+  email: document.getElementById("email").value
+
+},
+products: cart.map(item=>item.productId)
+
+  }
+  
+  fetch("http://localhost:3000/api/products/order",{method:"POST",body:JSON.stringify(order), headers: { "Content-Type": "application/json" }})
+  .then(response=>response.json())
+  .then(data=>{
+    localStorage.clear();
+    localStorage.setItem("orderId",data.orderId)
+    window.location.href = "confirmation.html";
+
+  })
+  .catch(error=>console.log(error))
+
+      //Create an object item JSON to stock all the datas in the local storage
+    //localStorage.clear();
+
+    //const send = {
+      //productLocalStorage,
+      //contact
+    //}
+   
+  
+  })
+  
+
+
--- front/js/script.js
+++ front/js/script.js
@@ -1,34 +1,28 @@
 const itemsInDocument = document.getElementById('items')
+let arrayOfItems = [];
 const fetchProducts = async() =>{
 fetch("http://localhost:3000/api/products")
 .then(response =>{return response.json()})
 .then(products=>{
-
-    itemsInDocument.innerHTML = products.map(product =>
-        `
-         <a href="product.html?id=${product._id}">
-        <article>
-          <img src=${product.imageUrl} alt=${product.altTxt}>
-          <h3 class="productName">${product.name}</h3>
-          <p class="productDescription">${product.description}</p>
-        </article>  
-      </a>
-          `
-        )
-
-
+  for (let a =0; a < products.length; a++){
+     arrayOfItems+=`
+    <a href="product.html?id=${products[a]._id}">
+    <article>
+      <img src=${products[a].imageUrl} alt=${products[a].altTxt}>
+      <h3 class="productName">${products[a].name}</h3>
+      <p class="productDescription">${products[a].description}</p>
+    </article>  
+  </a>
+    `
+    itemsInDocument.innerHTML = arrayOfItems;
+  }  
+          
 })
 
 }
 
+
 fetchProducts()
-
-
-
-
-
-
-
 
  
  
